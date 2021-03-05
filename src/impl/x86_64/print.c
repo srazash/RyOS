0b2c97a51dca5de8d2531fbe0efe7041c2d413f8
Ryan Shaw-Harrison <srazash@protonmail.com>
2021-03-05 17:53:45 +0000

    kernel update
    kernel coding in progress

--- src/impl/x86_64/print.c
+++ src/impl/x86_64/print.c
@@ -13,6 +13,71 @@ size_t col = 0;
 size_t row = 0;
 uint8_t colour = PRINT_COLOUR_WHITE | PRIT_COLOUR_WHITE << 4;
 
-voud print_clear() {
-    for (size_t i = 0, )
+void clear_row(size_t row) {
+    struct Char empty = (struct Char) {
+        character: ' ',
+        colour: colour,
+    }
+
+    for (size_t col = 0; col < NUM_COLS; cols++) {
+        buffer[col + NUM_COLS * row] = empty;
+    }
+}
+
+void print_clear() {
+    for (size_t i = 0; i < NUM_ROWS; i++) {
+        clear_row(i);
+    }
+}
+
+void print_newline() {
+    col = 0;
+
+    if (row < NUM_ROWS -1) {
+        row++;
+        return;
+    }
+
+    for (size_t row =1; row < NUM_ROWS; row++) {
+        for (size_t col = 0; col < NUM_COLS; col++) {
+            struct Char character = buffer[col + NUM_COLS * row];
+            buffer[col + NUM_COLS * (row -1)] = character;
+        }
+    }
+
+    clear_row(NUM_COLS - 1);
+}
+
+void print_char(char character) {
+    if (character == '\n') {
+        print_newline();
+        return;
+    }
+
+    if (col > NUM_COLS) {
+        print_newline();
+    }
+
+    buffer[col + NUM_COLS * row] = (struct Char) {
+        character: (uint8_t) character,
+        colour: colour,
+    };
+
+    col++;
+}
+
+void print_str(char* str) {
+    for (size_t i = 0; 1; i++) {
+        char character = (uint8_t) str[i];
+
+        if (character == '\0') {
+            return;
+        }
+
+        print_char(character);
+    }
+}
+
+void print_set_colour(uint8_t forground, uint8_t background) {
+    colour = forground + (background << 4);
 }
